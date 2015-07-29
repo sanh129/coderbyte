d79e0d3804999223326e68a7d13a00d9fb3d2f8c
sanh129 <sanh129@naver.com>
2015-07-30 07:20:29 +0900

    Create TimeConvert.rb

---
+++ TimeConvert.rb
@@ -0,0 +1,12 @@
+def TimeConvert(num)
+
+ num = + divmod(num)
+  # code goes here
+  
+  return num 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)           
